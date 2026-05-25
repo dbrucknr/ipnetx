6b16aba3828cde6786601f2f14c48ce40c644ca4
Derek Bruckner <dbrucknr@umich.edu>
2026-05-25 18:45:38 -0400

    Add in a few methods and unit tests

--- src/range.rs
+++ src/range.rs
@@ -32,3 +32,55 @@ impl std::fmt::Display for IpRange {
         write!(f, "{}..{}", self.start, self.end)
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::net::Ipv4Addr;
+
+    #[test]
+    fn test_start() {
+        let range = IpRange::new(
+            IpAddr::V4(Ipv4Addr::new(1, 0, 0, 0)),
+            IpAddr::V4(Ipv4Addr::new(1, 255, 255, 255)),
+        );
+        assert_eq!(range.start(), IpAddr::V4(Ipv4Addr::new(1, 0, 0, 0)));
+    }
+
+    #[test]
+    fn test_end() {
+        let range = IpRange::new(
+            IpAddr::V4(Ipv4Addr::new(1, 0, 0, 0)),
+            IpAddr::V4(Ipv4Addr::new(1, 255, 255, 255)),
+        );
+        assert_eq!(range.end(), IpAddr::V4(Ipv4Addr::new(1, 255, 255, 255)));
+    }
+
+    #[test]
+    fn test_valid() {
+        let range = IpRange::new(
+            IpAddr::V4(Ipv4Addr::new(1, 0, 0, 0)),
+            IpAddr::V4(Ipv4Addr::new(1, 255, 255, 255)),
+        );
+        assert!(range.valid());
+    }
+
+    #[test]
+    fn test_invalid() {
+        let range = IpRange::new(
+            IpAddr::V4(Ipv4Addr::new(1, 255, 255, 255)),
+            IpAddr::V4(Ipv4Addr::new(1, 0, 0, 0)),
+        );
+        assert!(!range.valid());
+    }
+
+    #[test]
+    fn test_contains() {
+        let range = IpRange::new(
+            IpAddr::V4(Ipv4Addr::new(1, 0, 0, 0)),
+            IpAddr::V4(Ipv4Addr::new(1, 255, 255, 255)),
+        );
+        assert!(range.contains(IpAddr::V4(Ipv4Addr::new(1, 1, 1, 1))));
+        assert!(!range.contains(IpAddr::V4(Ipv4Addr::new(2, 0, 0, 0))));
+    }
+}
