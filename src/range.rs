8d7d9ff73d8defa23842481c32c07ba6f9aa4e9d
Derek Bruckner <dbrucknr@umich.edu>
2026-05-26 09:32:03 -0400

    Implement static dispatch with trait; modify tests for generic

--- src/range.rs
+++ src/range.rs
@@ -1,43 +1,38 @@
-use std::net::{IpAddr, Ipv4Addr};
+use std::net::{Ipv4Addr, Ipv6Addr};
 
-#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
-pub enum IpFamilyMismatch {
-    V4V6,
-    V6V4,
+pub trait IpAddress:
+    PartialOrd + Copy + std::fmt::Display + std::fmt::Debug + PartialEq + Eq + std::hash::Hash
+{
+    fn is_unspecified(&self) -> bool;
 }
 
-// I wonder if the IpRange should accept a generic type that is either an IpAddr::V4 or IpAddr::V6?
-// Perhaps I should also consider prefix ranges?
-#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
-pub struct IpRange {
-    start: IpAddr,
-    end: IpAddr,
-}
-
-impl Default for IpRange {
-    fn default() -> Self {
-        Self {
-            start: IpAddr::V4(Ipv4Addr::UNSPECIFIED),
-            end: IpAddr::V4(Ipv4Addr::UNSPECIFIED),
-        }
+impl IpAddress for Ipv4Addr {
+    fn is_unspecified(&self) -> bool {
+        Ipv4Addr::is_unspecified(self)
     }
 }
 
-impl IpRange {
-    pub fn new(start: IpAddr, end: IpAddr) -> Result<Self, IpFamilyMismatch> {
-        match (&start, &end) {
-            (IpAddr::V4(_), IpAddr::V6(_)) => return Err(IpFamilyMismatch::V4V6),
-            (IpAddr::V6(_), IpAddr::V4(_)) => return Err(IpFamilyMismatch::V6V4),
-            _ => {}
-        }
-        Ok(Self { start, end })
+impl IpAddress for Ipv6Addr {
+    fn is_unspecified(&self) -> bool {
+        Ipv6Addr::is_unspecified(self)
+    }
+}
+
+pub struct IpRange<A: IpAddress> {
+    start: A,
+    end: A,
+}
+
+impl<A: IpAddress> IpRange<A> {
+    pub fn new(start: A, end: A) -> Self {
+        Self { start, end }
     }
 
-    pub fn start(&self) -> IpAddr {
+    pub fn start(&self) -> A {
         self.start
     }
 
-    pub fn end(&self) -> IpAddr {
+    pub fn end(&self) -> A {
         self.end
     }
 
@@ -45,39 +40,28 @@ impl IpRange {
         self.start <= self.end
     }
 
-    pub fn contains(&self, ip: IpAddr) -> bool {
+    pub fn contains(&self, ip: A) -> bool {
         self.is_valid() && self.start <= ip && ip <= self.end
     }
 
     pub fn is_zero(&self) -> bool {
-        match (self.start, self.end) {
-            (IpAddr::V4(s), IpAddr::V4(e)) => s.is_unspecified() && e.is_unspecified(),
-            (IpAddr::V6(s), IpAddr::V6(e)) => s.is_unspecified() && e.is_unspecified(),
-            _ => false,
-        }
+        self.start.is_unspecified() && self.end.is_unspecified()
     }
 
-    pub fn overlaps(&self, other: &IpRange) -> bool {
-        if !self.is_valid() || !other.is_valid() {
-            return false;
-        }
-        match (&self.start, &other.start) {
-            (IpAddr::V4(_), IpAddr::V6(_)) | (IpAddr::V6(_), IpAddr::V4(_)) => return false,
-            _ => {}
-        }
-        self.end >= other.start && self.start <= other.end
-    }
-
-    pub fn prefixes(&self) {
-        todo!()
+    pub fn overlaps(&self, other: &IpRange<A>) -> bool {
+        self.is_valid() && other.is_valid() && self.end >= other.start && self.start <= other.end
     }
 
     pub fn prefix(&self) {
         todo!()
     }
+
+    pub fn prefixes(&self) {
+        todo!()
+    }
 }
 
-impl std::fmt::Display for IpRange {
+impl<A: IpAddress> std::fmt::Display for IpRange<A> {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         write!(f, "{}..{}", self.start, self.end)
     }
@@ -88,231 +72,238 @@ mod tests {
     use super::*;
     use std::net::{Ipv4Addr, Ipv6Addr};
 
+    // --- Construction ---
+
+    // cargo test range::tests::test_v4_new
     #[test]
     fn test_v4_new() {
-        let range = IpRange::new(
-            IpAddr::V4(Ipv4Addr::new(1, 0, 0, 0)),
-            IpAddr::V4(Ipv4Addr::new(1, 255, 255, 255)),
-        );
-        assert!(range.is_ok());
-    }
-
-    #[test]
-    fn test_new_v4v6() {
-        let range = IpRange::new(
-            IpAddr::V4(Ipv4Addr::new(1, 0, 0, 0)),
-            IpAddr::V6(Ipv6Addr::new(0, 0, 0, 0, 0, 0, 0, 1)),
-        );
-        assert!(range.is_err());
-        assert_eq!(range.unwrap_err(), IpFamilyMismatch::V4V6);
-    }
-
-    #[test]
-    fn test_new_v6v4() {
-        let range = IpRange::new(
-            IpAddr::V6(Ipv6Addr::new(0, 0, 0, 0, 0, 0, 0, 1)),
-            IpAddr::V4(Ipv4Addr::new(1, 0, 0, 0)),
-        );
-        assert!(range.is_err());
-        assert_eq!(range.unwrap_err(), IpFamilyMismatch::V6V4);
-    }
-
-    #[test]
-    fn test_v4_start() {
-        let range = IpRange::new(
-            IpAddr::V4(Ipv4Addr::new(1, 0, 0, 0)),
-            IpAddr::V4(Ipv4Addr::new(1, 255, 255, 255)),
-        )
-        .unwrap();
-
-        assert_eq!(range.start(), IpAddr::V4(Ipv4Addr::new(1, 0, 0, 0)));
-    }
-
-    #[test]
-    fn test_v4_end() {
-        let range = IpRange::new(
-            IpAddr::V4(Ipv4Addr::new(1, 0, 0, 0)),
-            IpAddr::V4(Ipv4Addr::new(1, 255, 255, 255)),
-        )
-        .unwrap();
-
-        assert_eq!(range.end(), IpAddr::V4(Ipv4Addr::new(1, 255, 255, 255)));
-    }
-
-    #[test]
-    fn test_v4_valid() {
-        let range = IpRange::new(
-            IpAddr::V4(Ipv4Addr::new(1, 0, 0, 0)),
-            IpAddr::V4(Ipv4Addr::new(1, 255, 255, 255)),
-        )
-        .unwrap();
-
+        let range =
+            IpRange::<Ipv4Addr>::new(Ipv4Addr::new(1, 0, 0, 0), Ipv4Addr::new(1, 255, 255, 255));
         assert!(range.is_valid());
     }
 
+    // cargo test range::tests::test_v6_new
+    #[test]
+    fn test_v6_new() {
+        let range = IpRange::new(
+            Ipv6Addr::new(0x2001, 0xdb8, 0, 0, 0, 0, 0, 1),
+            Ipv6Addr::new(0x2001, 0xdb8, 0, 0, 0, 0, 0, 0xff),
+        );
+        assert!(range.is_valid());
+    }
+
+    // --- Start / End ---
+
+    // cargo test range::tests::test_v4_start
+    #[test]
+    fn test_v4_start() {
+        let range = IpRange::new(Ipv4Addr::new(1, 0, 0, 0), Ipv4Addr::new(1, 255, 255, 255));
+        assert_eq!(range.start(), Ipv4Addr::new(1, 0, 0, 0));
+    }
+
+    // cargo test range::tests::test_v4_end
+    #[test]
+    fn test_v4_end() {
+        let range = IpRange::new(Ipv4Addr::new(1, 0, 0, 0), Ipv4Addr::new(1, 255, 255, 255));
+        assert_eq!(range.end(), Ipv4Addr::new(1, 255, 255, 255));
+    }
+
+    // --- Validity ---
+
+    // cargo test range::tests::test_v4_valid
+    #[test]
+    fn test_v4_valid() {
+        let range = IpRange::new(Ipv4Addr::new(1, 0, 0, 0), Ipv4Addr::new(1, 255, 255, 255));
+        assert!(range.is_valid());
+    }
+
+    // cargo test range::tests::test_v4_invalid
     #[test]
     fn test_v4_invalid() {
-        let range = IpRange::new(
-            IpAddr::V4(Ipv4Addr::new(1, 255, 255, 255)),
-            IpAddr::V4(Ipv4Addr::new(1, 0, 0, 0)),
-        )
-        .unwrap();
-
+        let range = IpRange::new(Ipv4Addr::new(1, 255, 255, 255), Ipv4Addr::new(1, 0, 0, 0));
         assert!(!range.is_valid());
     }
 
+    // cargo test range::tests::test_v6_valid
+    #[test]
+    fn test_v6_valid() {
+        let range = IpRange::new(
+            Ipv6Addr::new(0, 0, 0, 0, 0, 0, 0, 0),
+            Ipv6Addr::new(0, 0, 0, 0, 0, 0, 0, 0),
+        );
+        assert!(range.is_valid());
+    }
+
+    // cargo test range::tests::test_v6_invalid
+    #[test]
+    fn test_v6_invalid() {
+        let range = IpRange::new(
+            Ipv6Addr::new(0, 0, 0, 0, 0, 0, 0, 1),
+            Ipv6Addr::new(0, 0, 0, 0, 0, 0, 0, 0),
+        );
+        assert!(!range.is_valid());
+    }
+
+    // --- Contains ---
+
+    // cargo test range::tests::test_v4_contains
     #[test]
     fn test_v4_contains() {
-        let range = IpRange::new(
-            IpAddr::V4(Ipv4Addr::new(1, 0, 0, 0)),
-            IpAddr::V4(Ipv4Addr::new(1, 255, 255, 255)),
-        )
-        .unwrap();
-
-        assert!(range.contains(IpAddr::V4(Ipv4Addr::new(1, 1, 1, 1))));
-        assert!(!range.contains(IpAddr::V4(Ipv4Addr::new(2, 0, 0, 0))));
+        let range = IpRange::new(Ipv4Addr::new(1, 0, 0, 0), Ipv4Addr::new(1, 255, 255, 255));
+        assert!(range.contains(Ipv4Addr::new(1, 1, 1, 1)));
+        assert!(!range.contains(Ipv4Addr::new(2, 0, 0, 0)));
     }
 
+    // cargo test range::tests::test_v4_contains_boundaries
+    #[test]
+    fn test_v4_contains_boundaries() {
+        let range = IpRange::new(Ipv4Addr::new(1, 0, 0, 0), Ipv4Addr::new(1, 255, 255, 255));
+
+        assert!(range.contains(Ipv4Addr::new(1, 0, 0, 0))); // start inclusive
+        assert!(range.contains(Ipv4Addr::new(1, 255, 255, 255))); // end inclusive
+
+        assert!(!range.contains(Ipv4Addr::new(0, 255, 255, 255))); // just before start
+        assert!(!range.contains(Ipv4Addr::new(2, 0, 0, 0))); // just after end
+    }
+
+    // cargo test range::tests::test_v4_contains_start_eq_end
     #[test]
     fn test_v4_contains_start_eq_end() {
-        let range = IpRange::new(
-            IpAddr::V4(Ipv4Addr::new(1, 0, 0, 0)),
-            IpAddr::V4(Ipv4Addr::new(1, 0, 0, 0)),
-        )
-        .unwrap();
-
-        assert!(range.contains(IpAddr::V4(Ipv4Addr::new(1, 0, 0, 0))));
+        let range = IpRange::new(Ipv4Addr::new(1, 0, 0, 0), Ipv4Addr::new(1, 0, 0, 0));
+        assert!(range.contains(Ipv4Addr::new(1, 0, 0, 0)));
     }
 
+    // cargo test range::tests::test_v4_invalid_contains
     #[test]
     fn test_v4_invalid_contains() {
-        let range = IpRange::new(
-            IpAddr::V4(Ipv4Addr::new(1, 255, 255, 255)),
-            IpAddr::V4(Ipv4Addr::new(1, 0, 0, 0)),
-        )
-        .unwrap();
-
-        assert!(!range.contains(IpAddr::V4(Ipv4Addr::new(1, 1, 1, 1))));
-        assert!(!range.contains(IpAddr::V4(Ipv4Addr::new(2, 0, 0, 0))));
+        let range = IpRange::new(Ipv4Addr::new(1, 255, 255, 255), Ipv4Addr::new(1, 0, 0, 0));
+        assert!(!range.contains(Ipv4Addr::new(1, 1, 1, 1)));
+        assert!(!range.contains(Ipv4Addr::new(2, 0, 0, 0)));
     }
 
+    // cargo test range::tests::test_v6_contains
     #[test]
-    fn test_v4_overlaps() {
+    fn test_v6_contains() {
         let range = IpRange::new(
-            IpAddr::V4(Ipv4Addr::new(1, 0, 0, 0)),
-            IpAddr::V4(Ipv4Addr::new(1, 255, 255, 255)),
-        )
-        .unwrap();
-
-        assert!(
-            range.overlaps(
-                &IpRange::new(
-                    IpAddr::V4(Ipv4Addr::new(1, 1, 1, 1)),
-                    IpAddr::V4(Ipv4Addr::new(1, 255, 255, 255)),
-                )
-                .unwrap()
-            )
+            Ipv6Addr::new(0, 0, 0, 0, 0, 0, 0, 0),
+            Ipv6Addr::new(0, 0, 0, 0, 0, 0, 0, 0),
         );
+        assert!(range.contains(Ipv6Addr::new(0, 0, 0, 0, 0, 0, 0, 0)));
     }
 
+    // cargo test range::tests::test_v6_invalid_contains
     #[test]
-    fn test_v4_overlaps_no_overlap() {
+    fn test_v6_invalid_contains() {
         let range = IpRange::new(
-            IpAddr::V4(Ipv4Addr::new(1, 0, 0, 0)),
-            IpAddr::V4(Ipv4Addr::new(1, 255, 255, 255)),
-        )
-        .unwrap();
-
-        assert!(
-            !range.overlaps(
-                &IpRange::new(
-                    IpAddr::V4(Ipv4Addr::new(2, 0, 0, 0)),
-                    IpAddr::V4(Ipv4Addr::new(2, 255, 255, 255)),
-                )
-                .unwrap()
-            )
+            Ipv6Addr::new(0, 0, 0, 0, 0, 0, 0, 0),
+            Ipv6Addr::new(0, 0, 0, 0, 0, 0, 0, 0),
         );
+        assert!(!range.contains(Ipv6Addr::new(0, 0, 0, 0, 0, 0, 0, 1)));
     }
 
-    #[test]
-    fn test_v6_overlaps() {
-        let range = IpRange::new(
-            IpAddr::V6(Ipv6Addr::new(0, 0, 0, 0, 0, 0, 0, 1)),
-            IpAddr::V6(Ipv6Addr::new(0, 0, 0, 0, 0, 0, 0, 255)),
-        )
-        .unwrap();
+    // --- Zero / Unspecified ---
 
-        assert!(
-            range.overlaps(
-                &IpRange::new(
-                    IpAddr::V6(Ipv6Addr::new(0, 0, 0, 0, 0, 0, 0, 1)),
-                    IpAddr::V6(Ipv6Addr::new(0, 0, 0, 0, 0, 0, 0, 255)),
-                )
-                .unwrap()
-            )
-        );
-    }
-
-    #[test]
-    fn test_v6_overlaps_no_overlap() {
-        let range = IpRange::new(
-            IpAddr::V6(Ipv6Addr::new(0, 0, 0, 0, 0, 0, 0, 1)),
-            IpAddr::V6(Ipv6Addr::new(0, 0, 0, 0, 0, 0, 0, 255)),
-        )
-        .unwrap();
-
-        assert!(
-            !range.overlaps(
-                &IpRange::new(
-                    IpAddr::V6(Ipv6Addr::new(1, 0, 0, 0, 0, 0, 0, 1)),
-                    IpAddr::V6(Ipv6Addr::new(1, 0, 0, 0, 0, 0, 0, 255)),
-                )
-                .unwrap()
-            )
-        );
-    }
-
+    // cargo test range::tests::test_v4_is_zero
     #[test]
     fn test_v4_is_zero() {
-        let range = IpRange::new(
-            IpAddr::V4(Ipv4Addr::new(0, 0, 0, 0)),
-            IpAddr::V4(Ipv4Addr::new(0, 0, 0, 0)),
-        )
-        .unwrap();
-
+        let range = IpRange::new(Ipv4Addr::UNSPECIFIED, Ipv4Addr::UNSPECIFIED);
         assert!(range.is_zero());
     }
 
+    // cargo test range::tests::test_v6_is_zero
     #[test]
     fn test_v6_is_zero() {
-        let range = IpRange::new(
-            IpAddr::V6(Ipv6Addr::new(0, 0, 0, 0, 0, 0, 0, 0)),
-            IpAddr::V6(Ipv6Addr::new(0, 0, 0, 0, 0, 0, 0, 0)),
-        )
-        .unwrap();
-
+        let range = IpRange::new(Ipv6Addr::UNSPECIFIED, Ipv6Addr::UNSPECIFIED);
         assert!(range.is_zero());
     }
 
+    // cargo test range::tests::test_v4_is_not_zero
+    #[test]
+    fn test_v4_is_not_zero() {
+        let range = IpRange::new(Ipv4Addr::new(1, 0, 0, 0), Ipv4Addr::new(1, 255, 255, 255));
+        assert!(!range.is_zero());
+    }
+
+    // cargo test range::tests::test_v6_is_not_zero
+    #[test]
+    fn test_v6_is_not_zero() {
+        let range = IpRange::new(
+            Ipv6Addr::new(0, 0, 0, 0, 0, 0, 0, 1),
+            Ipv6Addr::new(0, 0, 0, 0, 0, 0, 0, 2),
+        );
+        assert!(!range.is_zero());
+    }
+
+    // --- Overlaps ---
+
+    // cargo test range::tests::test_v4_overlaps
+    #[test]
+    fn test_v4_overlaps() {
+        let a = IpRange::new(Ipv4Addr::new(1, 0, 0, 0), Ipv4Addr::new(1, 255, 255, 255));
+        let b = IpRange::new(Ipv4Addr::new(1, 1, 1, 1), Ipv4Addr::new(1, 255, 255, 255));
+        assert!(a.overlaps(&b));
+    }
+
+    // cargo test range::tests::test_v4_overlaps_no_overlap
+    #[test]
+    fn test_v4_overlaps_no_overlap() {
+        let a = IpRange::new(Ipv4Addr::new(1, 0, 0, 0), Ipv4Addr::new(1, 255, 255, 255));
+        let b = IpRange::new(Ipv4Addr::new(2, 0, 0, 0), Ipv4Addr::new(2, 255, 255, 255));
+        assert!(!a.overlaps(&b));
+    }
+
+    // cargo test range::tests::test_v4_overlaps_invalid_range
+    #[test]
+    fn test_v4_overlaps_invalid_range() {
+        let valid = IpRange::new(Ipv4Addr::new(1, 0, 0, 0), Ipv4Addr::new(1, 255, 255, 255));
+        let invalid = IpRange::new(Ipv4Addr::new(1, 255, 255, 255), Ipv4Addr::new(1, 0, 0, 0));
+        assert!(!valid.overlaps(&invalid));
+    }
+
+    // cargo test range::tests::test_v6_overlaps
+    #[test]
+    fn test_v6_overlaps() {
+        let a = IpRange::new(
+            Ipv6Addr::new(0, 0, 0, 0, 0, 0, 0, 1),
+            Ipv6Addr::new(0, 0, 0, 0, 0, 0, 0, 255),
+        );
+        let b = IpRange::new(
+            Ipv6Addr::new(0, 0, 0, 0, 0, 0, 0, 1),
+            Ipv6Addr::new(0, 0, 0, 0, 0, 0, 0, 255),
+        );
+        assert!(a.overlaps(&b));
+    }
+
+    // cargo test range::tests::test_v6_overlaps_no_overlap
+    #[test]
+    fn test_v6_overlaps_no_overlap() {
+        let a = IpRange::new(
+            Ipv6Addr::new(0, 0, 0, 0, 0, 0, 0, 1),
+            Ipv6Addr::new(0, 0, 0, 0, 0, 0, 0, 255),
+        );
+        let b = IpRange::new(
+            Ipv6Addr::new(1, 0, 0, 0, 0, 0, 0, 1),
+            Ipv6Addr::new(1, 0, 0, 0, 0, 0, 0, 255),
+        );
+        assert!(!a.overlaps(&b));
+    }
+
+    // --- Display ---
+
+    // cargo test range::tests::test_v4_display
     #[test]
     fn test_v4_display() {
-        let range = IpRange::new(
-            IpAddr::V4(Ipv4Addr::new(1, 0, 0, 0)),
-            IpAddr::V4(Ipv4Addr::new(1, 255, 255, 255)),
-        )
-        .unwrap();
-
+        let range = IpRange::new(Ipv4Addr::new(1, 0, 0, 0), Ipv4Addr::new(1, 255, 255, 255));
         assert_eq!(format!("{}", range), "1.0.0.0..1.255.255.255");
     }
 
+    // cargo test range::tests::test_v6_display
     #[test]
     fn test_v6_display() {
         let range = IpRange::new(
-            IpAddr::V6(Ipv6Addr::new(1, 0, 0, 0, 0, 0, 0, 1)),
-            IpAddr::V6(Ipv6Addr::new(1, 0, 0, 0, 0, 0, 0, 255)),
-        )
-        .unwrap();
-
+            Ipv6Addr::new(1, 0, 0, 0, 0, 0, 0, 1),
+            Ipv6Addr::new(1, 0, 0, 0, 0, 0, 0, 255),
+        );
         assert_eq!(format!("{}", range), "1::1..1::ff");
     }
 }
